395ca1b35248b0b21ced9cd00065e0f60c4adb38
majopri <166362054+majopri@users.noreply.github.com>
2024-10-03 20:58:47 -0300

    Add files via upload

---
+++ ListaPOOex3/ListaPOOex3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado2 qd=new Quadrado2();
+
+            Console.WriteLine("Digite o valor da diagonal: ");
+            qd.setDiagonal(int.Parse(Console.ReadLine()));
+
+            qd.calcularArea();
+
+            Console.WriteLine("A área do quadrado é: {0}", qd.getArea());
+        }
+    }
+}
---
+++ ListaPOOex3/ListaPOOex3/Quadrado2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex3
+{
+    internal class Quadrado2
+    {
+        private int d;
+        private double area;
+        public void setDiagonal(int d)
+
+            { this.d = d; }
+
+        public int getDiagonal()
+            { return this.d; }
+
+        public double getArea()
+            { return this.area; }
+
+        public void calcularArea()
+        {
+            this.area = Math.Pow(d, 2) /2;
+        }
+    }
+}
