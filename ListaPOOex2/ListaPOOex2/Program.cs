694847c12e82c832788dfd3a76f90f01ba40858d
majopri <166362054+majopri@users.noreply.github.com>
2024-10-03 20:27:59 -0300

    Add files via upload

---
+++ ListaPOOex2/ListaPOOex2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado qd = new Quadrado();
+
+            Console.WriteLine("Digite o valor do lado: ");
+            qd.setLado(int.Parse(Console.ReadLine()));
+
+            qd.calcularArea();
+
+            Console.WriteLine("A área do quadrado é: {0}", qd.getArea());
+        }
+    }
+}
---
+++ ListaPOOex2/ListaPOOex2/Quadrado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex2
+{
+    internal class Quadrado
+    {
+        private int l;
+        private double area;
+        public void setLado(int l)
+
+            { this.l = l; }
+
+        public int getLado()
+            { return this.l; }
+
+        public double getArea()
+            { return this.area; }
+
+        public void calcularArea()
+        {
+            this.area = Math.Pow(l, 2); }
+        
+        }
+
+    }
