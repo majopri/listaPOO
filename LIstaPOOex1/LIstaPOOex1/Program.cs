e7a29af2cb80190e2b774dff05f033c8492c3ebd
majopri <166362054+majopri@users.noreply.github.com>
2024-10-03 20:06:01 -0300

    Add files via upload

---
+++ LIstaPOOex1/LIstaPOOex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIstaPOOex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret;
+            ret = new Retangulo();
+
+            Console.WriteLine("Digite o valor da base do retâgulo: ");
+            ret.setBase(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o valor da altura do retângulo: ");
+            ret.setAltura(int.Parse(Console.ReadLine()));
+
+            ret.calcularArea();
+
+            Console.WriteLine("Área do retângulo: {0}", ret.getArea());
+            
+        }
+    }
+}
---
+++ LIstaPOOex1/LIstaPOOex1/Retangulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIstaPOOex1
+{
+    internal class Retangulo
+    {
+        private int b;
+        private int h;
+        private int area;
+        public void setBase(int b)
+
+        {
+            this.b = b;
+        }
+
+        public void setAltura(int h)
+        
+        {
+            this.h = h;
+        }
+
+        public int getBase()
+        {
+            return this.b;
+        }
+
+        public int getAltura()
+        {
+            return this.h;
+        }
+
+        public int getArea()
+        {
+            return this.area;
+        }
+
+        public void calcularArea()
+        {
+            this.area = this.b * this.h;
+        }
+    }
+}
