3faf930ca82e531cf6b415c86e15d5626a94b34e
majopri <166362054+majopri@users.noreply.github.com>
2024-10-10 20:18:41 -0300

    Add files via upload

---
+++ Dolar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex7
+{
+    internal class Dolar
+    {
+        private double d;
+        private double c;
+        private double r;
+
+        public void setDolar(double d)
+            { this.d = d; }
+
+        public void setCotacao(double c)
+            { this.c = c; }
+
+        public double getDolar()
+            { return this.d; }
+
+        public double getCotacao()
+            { return this.c; }
+
+        public double getReais()
+        { return this.r; }
+
+        public void calcularReais()
+        { this.r = this.d * this.c; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar valor;
+            valor = new Dolar();
+
+            Console.WriteLine(" Digite o valor da cotação do dólar:   ");
+            valor.setCotacao(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite a quatidade de dólares:  ");
+            valor.setDolar(double.Parse(Console.ReadLine()));
+
+            valor.calcularReais();
+
+            Console.Write(" O valor em Reais é:   ");
+            Console.WriteLine(valor.getReais());
+
+        }
+    }
+}
