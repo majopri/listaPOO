1f029775402db90e461e69f99048b14da18ade4c
majopri <166362054+majopri@users.noreply.github.com>
2024-10-10 19:51:26 -0300

    Add files via upload

---
+++ Lista1Ex4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo tri;
+            tri = new Triangulo();
+
+            Console.WriteLine("digite o valor da base: ");
+            tri.setBase(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("digite o valor da altura: ");
+            tri.setAltura(int.Parse(Console.ReadLine()));
+
+            tri.calcularArea();
+
+            Console.WriteLine("o valor da area do triangulo e: ");
+            Console.WriteLine(tri.getArea());
+
+
+
+
+
+        }
+    }
+}
