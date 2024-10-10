0ecbbdba48d67f9503b67ab4d089549cbfdd086b
majopri <166362054+majopri@users.noreply.github.com>
2024-10-10 20:16:31 -0300

    Add files via upload

---
+++ Lista1Ex8/Lista1Ex8/Maior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex8
+{
+    internal class Maior
+    {
+        private double v1;
+        private double v2;
+        private double mv;
+
+        public void setValor1(double v1)
+        { this.v1 = v1; }
+
+        public void setValor2(double v2)
+        { this.v2 = v2; }
+
+        public double getValor1()
+        { return this.v1; }
+
+        public double getValor2()
+        { return this.v2; }
+
+        public double getMaior()
+        { return this.mv; }
+
+        public void calcularMaior()
+        {
+            if (this.v1 > this.v2)
+
+            { this.mv = this.v1; }
+
+            else
+            {
+                this.mv = this.v2;
+            }
+
+
+        }
+    }
+}
---
+++ Lista1Ex8/Lista1Ex8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior m;
+            m = new Maior();
+
+            Console.WriteLine(" Digite o primeiro valor:  ");
+            m.setValor1(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine(" Digite o segundo valor:  ");
+            m.setValor2(double.Parse(Console.ReadLine()));
+
+            m.calcularMaior();
+            Console.WriteLine("O maior valor é:  ");
+            Console.WriteLine(m.getMaior());
+
+
+
+        }
+    }
+}
